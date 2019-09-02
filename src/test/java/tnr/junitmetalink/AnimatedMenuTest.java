1dd554a138703fff2b0647a46919c3de04c0921f
Kévin Ung <kung@nx-domain.lan>
2019-09-02 16:56:59 +0200

    First commit

---
+++ src/test/java/tnr/junitmetalink/AnimatedMenuTest.java
@@ -0,0 +1,42 @@
+package tnr.junitmetalink;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class AnimatedMenuTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.get("https://codepen.io/knyttneve/pen/LKrGBy");
+    }
+
+    @TFMetadata(key = "linked-TC", value = "b070ba1c-d7e6-4b22-8f0a-5c9cf82cab4f")
+    @Test
+    public void testMenu(){
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='result']")));
+        driver.findElement(By.xpath("//a[contains(text(),'Blog')]")).click();
+        String style = driver.findElement(By.xpath("//a[contains(text(),'Blog')]")).getAttribute("class");
+        Assertions.assertTrue(style.contains("is-active"), "Le bouton n'a pas été activé");
+        driver.findElement(By.xpath("//a[contains(text(),'About')]")).click();
+        style = driver.findElement(By.xpath("//a[contains(text(),'Blog')]")).getAttribute("class");
+        Assertions.assertTrue(!style.contains("is-active"), "Le bouton n'a pas été désactivé");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/tnr/junitmetalink/MutationObserverTest.java
@@ -0,0 +1,43 @@
+package tnr.junitmetalink;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.squashtest.ta.galaxia.squash.tf.galaxia.annotations.TFMetadata;
+
+import java.util.concurrent.TimeUnit;
+
+public class MutationObserverTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.get("https://codepen.io/dayvidwhy/pen/egdZyY");
+    }
+
+    @TFMetadata(key = "linked-TC", value = "82cfccfe-4f80-4381-aa57-a3b041b51953")
+    @Test
+    public void testMutation(){
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='result']")));
+        driver.findElement(By.xpath("//button[@id='red-button']")).click();
+        String style = driver.findElement(By.xpath("//p[@id='some-id']")).getAttribute("style");
+        Assertions.assertTrue(style.contains("color: red"), "Le texte n'a pas été changé en rouge");
+        driver.findElement(By.xpath("//button[@id='blue-button']")).click();
+        style = driver.findElement(By.xpath("//p[@id='some-id']")).getAttribute("style");
+        Assertions.assertTrue(style.contains("color: blue"), "Le texte n'a pas été changé en bleu");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+}
